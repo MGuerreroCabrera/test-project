3fde2ce9cfd2d61c32f337416d17414b4705b048
Manuel <manuel@guerrerocabrera.com>
2023-12-27 22:24:06 +0100

    Otro comentario

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const aplicacion = document.querySelector("#app");
 const img = document.createElement("img");
 
 img.src = "public/logo.png";
+console.log("Voy a introducir la imagen en el div app");
 
 aplicacion.appendChild(img);
 
