1524879849df3446461e84f3cb31625b10738ec8
Manuel <manuel@guerrerocabrera.com>
2023-12-27 22:38:53 +0100

    Nuevo cambio

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const aplicacion = document.querySelector("#app");
 
 const img = document.createElement("img");
 
-img.src = "public/logo.png";
+img.src = "./assets/logo.png";
 console.log("Voy a introducir la imagen en el div app");
 
 aplicacion.appendChild(img);
